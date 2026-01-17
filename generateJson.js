210abd51aa1e757248016cd545ae8295267672b6
h4sh5 <38898566+h4sh5@users.noreply.github.com>
2026-01-17 15:03:18 +1000

    yesterday, today and tomorrow

--- generateJson.js
+++ generateJson.js
@@ -1,17 +1,31 @@
 import * as ausHolidays from './ausHolidays.js';
 
-const today = new Date();
-const year = today.getFullYear();
+var today = new Date();
+var yesterday = new Date();
+var tomorrow = new Date();
+yesterday.setDate(yesterday.getDate() - 1);
+tomorrow.setDate(tomorrow.getDate() + 1);
+
+
+
 const states = new Array("NSW","QLD","VIC","ACT","TAS","NT","WA","SA");
 
 var todayHolidaysByState = {};
+var yesterdayHolidaysByState = {};
+var tomorrowHolidaysByState = {};
+
+// format is YYYY/MM/DD
+// getMonth() returns month index, needs to + 1
+todayHolidaysByState['date'] = `${today.getFullYear()}/${(today.getMonth()+1).toString().padStart(2, "0")}/${today.getDate().toString().padStart(2, "0")}`;
+yesterdayHolidaysByState['date'] = `${yesterday.getFullYear()}/${(yesterday.getMonth()+1).toString().padStart(2, "0")}/${yesterday.getDate().toString().padStart(2, "0")}`;
+tomorrowHolidaysByState['date'] = `${tomorrow.getFullYear()}/${(tomorrow.getMonth()+1).toString().padStart(2, "0")}/${tomorrow.getDate().toString().padStart(2, "0")}`;
 
 for (var state of states) {
-	// console.log(`today: ${today} ${state}`);
-	const pubHoliday = ausHolidays.getPublicHolidayFromDate(today, state);
-	todayHolidaysByState[state] = pubHoliday;
+	todayHolidaysByState[state] = ausHolidays.getPublicHolidayFromDate(today, state);
+	yesterdayHolidaysByState[state] = ausHolidays.getPublicHolidayFromDate(yesterday, state);
+	tomorrowHolidaysByState[state] = ausHolidays.getPublicHolidayFromDate(tomorrow, state); 
 }
 
-todayHolidaysByState['date'] = `${year}/${today.getMonth()+1}/${today.getDate()}`;
 
-console.log(JSON.stringify(todayHolidaysByState));
+console.log(JSON.stringify({"yesterday":yesterdayHolidaysByState, "today":todayHolidaysByState, "tomorrow":tomorrowHolidaysByState}));
+
